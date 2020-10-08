243d74d93d8d07e8dbd7f9dab0771682178251da
Gordon Yeong <gordon.yeong@broadsource.com.au>
2020-10-08 21:56:30 +1100

    write the place() function's test

--- test/toyrobot_test.exs
+++ test/toyrobot_test.exs
@@ -2,7 +2,8 @@ defmodule TOYROBOTTest do
   use ExUnit.Case
   doctest TOYROBOT
 
-  test "greets the world" do
-    assert TOYROBOT.hello() == :world
+  test "calling place without anything sets me at home" do
+    
+    assert TOYROBOT.place == %{ :x => 0, :y => 0 , :f => :north}
   end
 end
