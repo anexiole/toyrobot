6e127e4b135e03580915e7e99eaf84f2443e11dc
Gordon Yeong <gordon.yeong@broadsource.com.au>
2020-10-14 21:36:36 +1100

    update tests for left along with the ones for right

--- test/toyrobot_test.exs
+++ test/toyrobot_test.exs
@@ -51,6 +51,9 @@ defmodule TOYROBOTTest do
     assert TOYROBOT.left( %{:x => 3,:y => 0,:f => 'west'} ) == %{:x => 3,:y => 0,:f => 'west'}
   end
 
+  test "calling left when the robot is on the right most x-pane and facing south will be ignored" do
+    assert TOYROBOT.left( %{:x => 4,:y => 2,:f => 'south'} ) == %{:x => 4,:y =>2 ,:f => 'south'}
+  end
 
   test "calling left when facing north" do
     assert TOYROBOT.left( %{:x => 3,:y => 0,:f => 'north'} ) == %{:x => 2,:y => 0,:f => 'north'}
@@ -68,5 +71,37 @@ defmodule TOYROBOTTest do
     assert TOYROBOT.left( %{:x => 3,:y => 1,:f => 'west'} ) == %{:x => 3,:y => 0,:f => 'west'}
   end
 
+ test "calling right when robot is on the bottom most y-pane and facing east will be ignored" do
+    assert TOYROBOT.right( %{:x => 0,:y => 4,:f => 'east'} ) == %{:x => 2,:y => 4,:f => 'east'}
+  end
+
+  test "calling right when robot is on the right most x-pane and facing north will be ignored" do
+    assert TOYROBOT.right( %{:x => 4,:y => 4,:f => 'north'} ) == %{:x => 4,:y => 4,:f => 'north'}
+  end
+
+  test "calling right when the robot is on the top most y-pane and facing west will be ignored" do
+    assert TOYROBOT.right( %{:x => 3,:y => 4,:f => 'west'} ) == %{:x => 3,:y => 4,:f => 'west'}
+  end
+
+  test "calling right when the robot is on the bottom most x-pane and facing south will be ignored" do
+    assert TOYROBOT.right( %{:x => 0,:y => 3,:f => 'west'} ) == %{:x => 0,:y =>3,:f => 'south'}
+  end
+
+
+  test "calling right when facing north" do
+    assert TOYROBOT.right( %{:x => 3,:y => 0,:f => 'north'} ) == %{:x => 2,:y => 0,:f => 'north'}
+  end
+
+   test "calling right when facing south" do
+    assert TOYROBOT.right( %{:x => 3,:y => 0,:f => 'south'} ) == %{:x => 4,:y => 0,:f => 'south'}
+  end 
+
+    test "calling right when facing east" do
+    assert TOYROBOT.right( %{:x => 3,:y => 1,:f => 'east'} ) == %{:x => 3,:y => 2,:f => 'east'}
+  end
+
+  test "calling right when facing west" do
+    assert TOYROBOT.right( %{:x => 3,:y => 1,:f => 'west'} ) == %{:x => 3,:y => 0,:f => 'west'}
+  end
 
 end
